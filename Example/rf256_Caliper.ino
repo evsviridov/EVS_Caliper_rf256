a70ee44cdd46b244804f696eb0fd98b78feaafc3
evsviridov <32343616+evsviridov@users.noreply.github.com>
2017-09-28 21:13:31 +0300

    Add files via upload

--- Example/rf256_Caliper.ino
+++ Example/rf256_Caliper.ino
@@ -1,22 +1,21 @@
 #include <evs_Caliper_rf256.h>
 
+#define NUM_OF_CALIPERS 2
+uint8_t address[NUM_OF_CALIPERS]={1,2};
+Caliper_RF256 rf256[NUM_OF_CALIPERS];
+
+HardwareSerial *p_Serial = &Serial1;
 #define RS485_TX_RX_PIN 7
 
-#define RS485_PORT_SPEED 9600
-
-Caliper_RF256 rf256[2];
-
-int counter = 0;
+#define PORT_SPEED 9600
 
 void setup()
 {
   Serial.begin(9600);
-  Serial1.begin(RS485_PORT_SPEED);
 
-  rf256[0].begin(1, RS485_TX_RX_PIN);
-  rf256[1].begin(2, RS485_TX_RX_PIN);
-  for (int i = 0; i < 2; ++i)
+  for (int i = 0; i < NUM_OF_CALIPERS; ++i)
   {
+    rf256[i].begin(address[i], p_Serial, RS485_TX_RX_PIN, PORT_SPEED);
     rf256[i].doIDN();
     rf256[i].printIDN();
   }
@@ -24,19 +23,17 @@ void setup()
 
 void loop()
 {
+  static int counter = 0;
   float val;
-  Serial.print(++counter);
-  Serial.print(":\t");
-  for (int i = 0; i < 2; ++i)
+  Serial.print(++counter); Serial.print(":\t");
+  for (int i = 0; i < NUM_OF_CALIPERS; ++i)
   {
-    //    val = rf256[i].doMeas() * 0.0001f;
     val = rf256[i].doMeasFloat();
     Serial.print((String) "x" + (i + 1) + "=" + String(val, 3) );
-    if ( i != 1) Serial.print("\t");
+    if ( i < (NUM_OF_CALIPERS-1)) Serial.print("\t");
   }
   Serial.println();
 
-
-  delay(1000);
+  delay(500);
 }
 
